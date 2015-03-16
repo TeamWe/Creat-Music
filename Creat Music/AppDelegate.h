6031e7ba2697391e40d5c3e85109a64bb6561bb0
TeamWe <xujiguang@xiyoumatoiMac-2.local>
2015-03-16 20:23:08 +0800

    Initial Commit

---
+++ Creat Music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Creat Music
+//
+//  Created by 徐纪光 on 15/3/16.
+//  Copyright (c) 2015年 徐纪光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
